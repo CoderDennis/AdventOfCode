b7551adf28c1d80ffbb9b1637e6efc33d211eaae
Dennis Palmer <dennis.palmer@mitigram.com>
2022-12-06 07:27:16 +0100

    day 6 with refactoring

--- lib/2022/6.ex
+++ lib/2022/6.ex
@@ -2,8 +2,21 @@ import AOC
 
 aoc 2022, 6 do
   def p1 do
+    input_string()
+    |> find_unique_chunk(4)
   end
 
   def p2 do
+    input_string()
+    |> find_unique_chunk(14)
   end
+
+  defp find_unique_chunk(str, size) do
+    (str
+    |> String.codepoints()
+    |> Enum.chunk_every(size, 1, :discard)
+    |> Enum.take_while(fn list -> (MapSet.new(list) |> MapSet.size) != size end)
+    |> Enum.count()) + size
+  end
+
 end
