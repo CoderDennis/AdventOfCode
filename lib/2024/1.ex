a0ce35561f9a7a3707a63533d70a9c3f15872cff
Dennis Palmer <dodava@gmail.com>
2024-12-02 11:40:51 +0100

    finished day 1

--- lib/2024/1.ex
+++ lib/2024/1.ex
@@ -2,26 +2,39 @@ import AOC
 
 aoc 2024, 1 do
   def p1(input) do
-    {left, right} =
-      input
-      |> String.split("\n")
-      |> Enum.reduce({[], []}, fn line, {acc1, acc2} ->
-        [a, b] =
-          line
-          |> String.split(" ", trim: true)
-          |> Enum.map(&String.to_integer/1)
-
-        {[a | acc1], [b | acc2]}
-      end)
+    {left, right} = get_lists(input)
 
     Enum.sort(left)
     |> Enum.zip(Enum.sort(right))
-    |> IO.inspect()
     |> Enum.reduce(0, fn {l, r}, ans ->
-      ans + r - l
+      ans + abs(r - l)
     end)
   end
 
-  def p2(_input) do
+  def p2(input) do
+    {left, right} = get_lists(input)
+
+    right_counts =
+      Enum.reduce(right, %{}, fn x, counts ->
+        Map.update(counts, x, 1, fn count -> count + 1 end)
+      end)
+
+    left
+    |> Enum.reduce(0, fn x, ans ->
+      ans + x * Map.get(right_counts, x, 0)
+    end)
+  end
+
+  defp get_lists(input) do
+    input
+    |> String.split("\n")
+    |> Enum.reduce({[], []}, fn line, {acc1, acc2} ->
+      [a, b] =
+        line
+        |> String.split(" ", trim: true)
+        |> Enum.map(&String.to_integer/1)
+
+      {[a | acc1], [b | acc2]}
+    end)
   end
 end
