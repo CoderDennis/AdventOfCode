0e2c63f6a5d53cab3798f319877c6cd91569f61b
Dennis Palmer <dodava@gmail.com>
2024-12-10 10:02:34 +0100

    day 10, part 2

--- lib/2024/10.ex
+++ lib/2024/10.ex
@@ -4,11 +4,6 @@ alias Helpers.CoordinateMap
 
 aoc 2024, 10 do
   def p1(input) do
-    #     input = "0123
-    # 1234
-    # 8765
-    # 9876"
-
     map =
       input
       |> CoordinateMap.create(&String.to_integer/1)
@@ -21,8 +16,6 @@ aoc 2024, 10 do
     |> Enum.map(&find_reachable_9_height_positions(&1, map, MapSet.new()))
     |> Enum.map(&MapSet.size/1)
     |> Enum.sum()
-
-    # |> Enum.reduce(0, fn trailhead, count -> count + find_trails(trailhead, map) end)
   end
 
   @directions [{-1, 0}, {1, 0}, {0, -1}, {0, 1}]
@@ -31,8 +24,6 @@ aoc 2024, 10 do
     do: MapSet.put(result, position)
 
   def find_reachable_9_height_positions({{r, c}, height}, map, result) do
-    # IO.inspect({r, c})
-
     @directions
     |> Enum.map(fn {dr, dc} -> {r + dr, c + dc} end)
     |> Enum.filter(fn next_position ->
@@ -50,6 +41,32 @@ aoc 2024, 10 do
     end)
   end
 
-  def p2(_input) do
+  def p2(input) do
+    map =
+      input
+      |> CoordinateMap.create(&String.to_integer/1)
+
+    map
+    |> Enum.filter(fn
+      {_, 0} -> true
+      _ -> false
+    end)
+    |> Enum.reduce(0, fn trailhead, count -> count + find_trails(trailhead, map) end)
+  end
+
+  def find_trails({_, 9}, _map), do: 1
+
+  def find_trails({{r, c}, height}, map) do
+    @directions
+    |> Enum.map(fn {dr, dc} -> {r + dr, c + dc} end)
+    |> Enum.filter(fn next_position ->
+      case Map.get(map, next_position) do
+        next_height when next_height - 1 == height -> true
+        _ -> false
+      end
+    end)
+    |> Enum.reduce(0, fn next_position, count ->
+      count + find_trails({next_position, Map.get(map, next_position)}, map)
+    end)
   end
 end
