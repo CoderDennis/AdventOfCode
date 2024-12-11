dd16d2596b1510c7b17760cb5db9a1c46eb9d708
Dennis Palmer <dodava@gmail.com>
2024-12-11 23:14:56 +0100

    day 11 part 2 with memoize

--- lib/2024/11.ex
+++ lib/2024/11.ex
@@ -1,6 +1,8 @@
 import AOC
 
 aoc 2024, 11 do
+  use Memoize
+
   def p1(input) do
     do_blinks(input, 25)
   end
@@ -45,7 +47,46 @@ aoc 2024, 11 do
     {new_stones, new_stones}
   end
 
+  defmemo count_stones_for_blinks(_stone, 0) do
+    1
+  end
+
+  defmemo count_stones_for_blinks(0, blinks) do
+    count_stones_for_blinks(1, blinks - 1)
+  end
+
+  defmemo count_stones_for_blinks(stone, blinks) do
+    digit_count =
+      stone
+      |> :math.log10()
+      |> floor()
+      |> then(fn x -> x + 1 end)
+
+    case rem(digit_count, 2) do
+      0 ->
+        stone
+        |> Integer.digits()
+        |> Enum.split(div(digit_count, 2))
+        |> Tuple.to_list()
+        |> Enum.map(&Integer.undigits/1)
+
+      _ ->
+        [stone * 2024]
+    end
+    |> Enum.map(fn stone -> count_stones_for_blinks(stone, blinks - 1) end)
+    |> Enum.sum()
+  end
+
   def p2(input) do
-    do_blinks(input, 75)
+    # do_blinks(input, 75)
+
+    input
+    |> String.split(" ")
+    |> Enum.map(fn stone ->
+      stone
+      |> String.to_integer()
+      |> count_stones_for_blinks(75)
+    end)
+    |> Enum.sum()
   end
 end
