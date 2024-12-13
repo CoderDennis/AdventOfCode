58384cddeccb8f8c91d61738efad2292081d4f4f
Dennis Palmer <dodava@gmail.com>
2024-12-13 21:33:48 +0100

    day 13 -- math for the win

--- lib/2024/13.ex
+++ lib/2024/13.ex
@@ -1,48 +1,47 @@
 import AOC
 
 aoc 2024, 13 do
-  use Memoize
-
   def p1(input) do
-    # is the cheapest also the shortest?
-    # it doesn't seem like it because button A is more expensive
-
     input
     |> String.split("\n")
     |> Enum.chunk_every(3, 4)
     |> Enum.map(&play/1)
-    |> Enum.reject(&(&1 == :infinity))
     |> Enum.sum()
   end
 
-  def play([a, b, prize]) do
-    button_a = button(a)
-    button_b = button(b)
-    prize = prize(prize)
+  def play([a, b, prize], add_to_prize \\ 0) do
+    {ax, ay} = button_a = button(a)
+    {bx, by} = button_b = button(b)
+    {x, y} = prize(prize)
 
-    play(button_a, button_b, prize, 0, 0)
+    x = x + add_to_prize
+    y = y + add_to_prize
+
+    if Integer.mod(x, Integer.gcd(ax, bx)) == 0 and
+         Integer.mod(y, Integer.gcd(ay, by)) == 0 do
+      solve(button_a, button_b, {x, y})
+    else
+      0
+    end
   end
 
-  defmemo play(
-            {ax, ay} = button_a,
-            {bx, by} = button_b,
-            {prize_x, prize_y} = prize,
-            button_a_count,
-            button_b_count
-          ) do
-    case {ax * button_a_count + bx * button_b_count, ay * button_a_count + by * button_b_count} do
-      {^prize_x, ^prize_y} ->
-        button_a_count * 3 + button_b_count
+  def solve(
+        {ax, ay} = _button_a,
+        {bx, by} = _button_b,
+        {prize_x, prize_y} = _prize
+      ) do
+    # IO.inspect({button_a, button_b, prize})
 
-      {x, y} when x > prize_x or y > prize_y ->
-        :infinity
+    # https://www.google.com/search?q=94x%2B22y%3D8400%3B34x%2B67y%3D5400 -> select "solve using the elimination method"
+    a_presses = div(prize_x * by - prize_y * bx, ax * by - bx * ay)
+    b_presses = div(prize_y - ay * a_presses, by)
 
-      _ ->
-        [
-          play(button_a, button_b, prize, button_a_count + 1, button_b_count),
-          play(button_a, button_b, prize, button_a_count, button_b_count + 1)
-        ]
-        |> Enum.min()
+    # IO.inspect({a_presses, b_presses})
+    if ax * a_presses + bx * b_presses == prize_x and
+         ay * a_presses + by * b_presses == prize_y do
+      a_presses * 3 + b_presses
+    else
+      0
     end
   end
 
@@ -68,16 +67,7 @@ aoc 2024, 13 do
     input
     |> String.split("\n")
     |> Enum.chunk_every(3, 4)
-    |> Enum.map(&play_2/1)
-    |> Enum.reject(&(&1 == :infinity))
+    |> Enum.map(&play(&1, 10_000_000_000_000))
     |> Enum.sum()
-  end
-
-  def play_2([a, b, prize]) do
-    button_a = button(a)
-    button_b = button(b)
-    {x, y} = prize(prize)
-
-    play(button_a, button_b, {x + 10_000_000_000_000, y + 10_000_000_000_000}, 0, 0)
   end
 end
