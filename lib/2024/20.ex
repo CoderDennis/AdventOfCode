4cca68bc24e83b3caaebb5809a29c1537acc6a7b
Dennis Palmer <dodava@gmail.com>
2024-12-20 10:46:42 +0100

    day 20, path finding works, but need to uniquely identify cheats

--- lib/2024/20.ex
+++ lib/2024/20.ex
@@ -20,7 +20,7 @@ aoc 2024, 20 do
     picoseconds_with_no_cheat = times_to_exit_with_cheat(map, start_position, :used) |> Enum.at(0)
     picoseconds_with_cheat = times_to_exit_with_cheat(map, start_position)
 
-    IO.inspect({picoseconds_with_no_cheat, picoseconds_with_cheat})
+    # IO.inspect({picoseconds_with_no_cheat, picoseconds_with_cheat})
 
     picoseconds_with_cheat
     |> Enum.map(&(picoseconds_with_no_cheat - &1))
@@ -31,27 +31,25 @@ aoc 2024, 20 do
   def times_to_exit_with_cheat(map, start_position, cheat_status \\ :available) do
     q = :queue.new()
 
-    visited = MapSet.new([{start_position, cheat_status}])
+    q = :queue.in({start_position, 0, cheat_status, MapSet.new()}, q)
 
-    q = :queue.in({start_position, 0, cheat_status, []}, q)
-
-    Stream.unfold({q, visited}, fn {q, visited} ->
+    Stream.unfold(q, fn q ->
       case :queue.out(q) do
-        {:empty, q} ->
-          {nil, {q, visited}}
+        {:empty, _q} ->
+          nil
 
         {{:value, {{x, y} = position, time_so_far, cheat_status, path}} = _value, q} ->
           # IO.inspect(value)
-          if cheat_status == :active do
-            IO.inspect({position, cheat_status})
-          end
+          # if cheat_status == :active do
+          #   IO.inspect({position, cheat_status})
+          # end
 
           if Map.get(map, position) == "E" do
-            {{time_so_far, Enum.reverse(path)}, {q, visited}}
+            {{time_so_far, Enum.reverse(path)}, q}
           else
-            {new_q, new_visited} =
+            new_q =
               @directions
-              |> Enum.reduce({q, visited}, fn {dx, dy} = _next_direction, {q, visited} ->
+              |> Enum.reduce(q, fn {dx, dy} = _next_direction, q ->
                 next_position = {x + dx, y + dy}
                 time = time_so_far + 1
 
@@ -59,21 +57,21 @@ aoc 2024, 20 do
                   can_move?(cheat_status, Map.get(map, next_position))
 
                 if Map.has_key?(map, next_position) and
-                     not MapSet.member?(visited, {next_position, next_cheat_status}) do
+                     not MapSet.member?(path, next_position) do
                   if can_move do
-                    {:queue.in(
-                       {next_position, time, next_cheat_status, [next_position | path]},
-                       q
-                     ), MapSet.put(visited, {next_position, next_cheat_status})}
+                    :queue.in(
+                      {next_position, time, next_cheat_status, MapSet.put(path, next_position)},
+                      q
+                    )
                   else
-                    {q, visited}
+                    q
                   end
                 else
-                  {q, visited}
+                  q
                 end
               end)
 
-            {nil, {new_q, new_visited}}
+            {nil, new_q}
           end
       end
     end)
