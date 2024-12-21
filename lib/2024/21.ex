3529b1cd13f6224ee05a87f7a6510d615bc8f979
Dennis Palmer <dodava@gmail.com>
2024-12-21 22:55:39 +0100

    day 21 part 2 non-working version save point

--- lib/2024/21.ex
+++ lib/2024/21.ex
@@ -83,9 +83,6 @@ aoc 2024, 21 do
 
   defmemo shortest_paths_to_button(keypad, start_position, button) do
     q = :queue.new()
-
-    # visited = MapSet.new([start_position])
-
     q = :queue.in({start_position, [], MapSet.new([start_position])}, q)
 
     button_paths =
@@ -140,20 +137,188 @@ aoc 2024, 21 do
     {button_position, shortest_paths}
   end
 
-  def p2(_input) do
-    # codes =
-    #   input
-    #   |> String.split("\n")
-    #   |> Enum.map(&String.codepoints/1)
+  def p2(input) do
+    codes =
+      input
+      |> String.split("\n")
+      |> Enum.map(&String.codepoints/1)
 
-    # numeric_keypad =
-    #   [["7", "8", "9"], ["4", "5", "6"], ["1", "2", "3"], [nil, "0", "A"]]
-    #   |> CoordinateMap.create_from_lists()
+    numeric_keypad =
+      [["7", "8", "9"], ["4", "5", "6"], ["1", "2", "3"], [nil, "0", "A"]]
+      |> CoordinateMap.create_from_lists()
 
-    # directional_keypad =
-    #   [[nil, "^", "A"], ["<", "v", ">"]]
-    #   |> CoordinateMap.create_from_lists()
+    directional_keypad =
+      [[nil, "^", "A"], ["<", "v", ">"]]
+      |> CoordinateMap.create_from_lists()
 
-    # find the path for each button one at a time all the way down in such a way that we can memoize the result
+    # possible approach:
+    # find the path for each button one at a time all the way down in such a way that we can memoize the result at each step
+    # codes
+    # |> Stream.map(fn code ->
+    #   number =
+    #     code
+    #     |> Enum.take(3)
+    #     |> Enum.map(&String.to_integer/1)
+    #     |> Integer.undigits()
+
+    #   length =
+    #     path_to_code_2(numeric_keypad, directional_keypad, code, 2)
+    #     |> elem(1)
+    #     |> Enum.map(&Enum.count/1)
+    #     |> Enum.min()
+
+    #   IO.inspect({length, number})
+    #   number * length
+    # end)
+    # |> Enum.sum()
+
+    shortest_paths_to_button_2(directional_keypad, {0, 2}, "<", 3)
+  end
+
+  def path_to_code_2(numeric_keypad, directional_keypad, code, robot_count) do
+    {numeric_a_position, _} =
+      numeric_keypad
+      |> Enum.find(&(elem(&1, 1) == "A"))
+
+    {directional_a_position, _} =
+      directional_keypad
+      |> Enum.find(&(elem(&1, 1) == "A"))
+
+    code
+    |> Enum.reduce({numeric_a_position, [[]]}, fn button, {start_position, paths} ->
+      {next_position, button_paths} =
+        shortest_paths_to_button(numeric_keypad, start_position, button)
+
+      shortest_paths =
+        button_paths
+        |> Enum.flat_map(fn button_path ->
+          paths
+          |> Enum.map(fn path ->
+            Enum.concat(path, button_path)
+          end)
+        end)
+
+      shortest_paths =
+        shortest_paths
+        |> Enum.map(fn path ->
+          path
+          |> Enum.reduce({directional_a_position, [[]]}, fn button, {start_position, paths} ->
+            {next_position, button_paths} =
+              shortest_paths_to_button_2(
+                directional_keypad,
+                start_position,
+                button,
+                robot_count
+              )
+
+            shortest_paths =
+              button_paths
+              |> Enum.flat_map(fn button_path ->
+                paths
+                |> Enum.map(fn path ->
+                  Enum.concat(path, button_path)
+                end)
+              end)
+
+            {next_position, shortest_paths}
+          end)
+        end)
+        |> Enum.map(&elem(&1, 1))
+
+      {next_position, shortest_paths}
+    end)
+  end
+
+  def shortest_paths_to_button_2(keypad, start_position, button, 0) do
+    # shortest_paths_to_button(keypad, start_position, button)
+    [{{-1, -1}, []}]
+  end
+
+  def shortest_paths_to_button_2(keypad, start_position, button, robot_count) do
+    IO.inspect({start_position, button, robot_count})
+    q = :queue.new()
+    q = :queue.in({start_position, [], MapSet.new([start_position])}, q)
+
+    button_paths =
+      Stream.unfold(q, fn q ->
+        case :queue.out(q) do
+          {:empty, _q} ->
+            nil
+
+          {{:value, {{x, y} = position, path, visited}}, q} ->
+            if Map.get(keypad, position) == button do
+              {{position, Enum.reverse(["A" | path])}, q}
+            else
+              q =
+                @directions
+                |> Enum.map(fn {{dx, dy}, label} ->
+                  {{x + dx, y + dy}, label}
+                end)
+                |> Enum.reject(fn {next, _label} ->
+                  MapSet.member?(visited, next) or Map.get(keypad, next) == nil
+                end)
+                |> Enum.reduce(q, fn {next, label}, q ->
+                  visited = MapSet.put(visited, next)
+                  :queue.in({next, [label | path], visited}, q)
+                end)
+
+              {nil, q}
+            end
+        end
+      end)
+      |> Enum.reject(&(&1 == nil))
+      |> Enum.uniq()
+      |> IO.inspect(label: "button_paths")
+
+    {button_position, _} =
+      Enum.at(button_paths, 0)
+
+    paths_with_lenghts =
+      button_paths
+      |> Enum.map(&elem(&1, 1))
+      |> Enum.map(fn path ->
+        {path, Enum.count(path)}
+      end)
+
+    # |> IO.inspect()
+
+    shortest_path_length =
+      paths_with_lenghts
+      |> Enum.map(&elem(&1, 1))
+      |> Enum.min()
+
+    shortest_paths =
+      paths_with_lenghts
+      |> Enum.filter(fn {_, len} -> len == shortest_path_length end)
+      |> Enum.map(&elem(&1, 0))
+
+    shortest_paths =
+      shortest_paths
+      |> IO.inspect(label: "shortest_paths")
+      |> Enum.flat_map(fn path ->
+        path
+        |> Enum.reduce({button_position, [[]]}, fn button, {start_position, paths} ->
+          IO.inspect({button, start_position, paths})
+
+          {next_position, button_paths} =
+            shortest_paths_to_button_2(keypad, start_position, button, robot_count - 1)
+            |> IO.inspect(label: "recursive call")
+
+          shortest_paths =
+            button_paths
+            |> Enum.flat_map(fn button_path ->
+              paths
+              |> Enum.map(fn path ->
+                Enum.concat(path, button_path)
+              end)
+            end)
+
+          {next_position, shortest_paths}
+        end)
+      end)
+      |> Enum.map(&elem(&1, 1))
+
+    {button_position, shortest_paths}
+    |> IO.inspect(label: "{button_position, shortest_paths}")
   end
 end
